ce1501bc35502d5a67cd1a2b962888f473c706d7
aburt11 <aburton.live@gmail.com>
2015-11-09 10:05:05 +1100

    initial commit

---
+++ build/core/src/com/roguelike/game/screens/GameScreen.java
@@ -0,0 +1,49 @@
+package com.roguelike.game.screens;
+
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by adam on 8/11/15.
+ */
+public class GameScreen extends Screen {
+
+    public GameScreen(){
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+    super.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ build/desktop/src/com/roguelike/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.roguelike.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.roguelike.game.RogueGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RogueGame(), config);
+	}
+}
